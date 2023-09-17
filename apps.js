4e5e9cb5b276fcf7f9185df4c057e19e4a92d66b
Muhammad Hamza <143834218+hhamza56@users.noreply.github.com>
2023-09-17 13:02:46 +0100

    complete

---
+++ apps.js
@@ -0,0 +1,135 @@
+
+// question 1//
+
+// var arr =["appale","orange","sami","hashir","hamza",]
+// { document.write("<select>" + "<option>" + arr[0] + "</option>" +  
+//   "<option>" + arr[1] + "</option>" +
+//  "<option>" + arr[2] + "</option>" +
+//  "<option>" + arr[3] + "</option>" +
+//  "<option>" + arr[4] + "</option>" +
+// "</select>")
+// }
+
+// question 2//
+
+// for( var i = 1;  i <=10; i++){
+//     document.write("welcome" + i+ "<br>"+"<br>")
+// }
+
+
+// question 3//
+
+// for( var i = 1;  i <=12; i++){
+//     document.write("javascript " + i+ "<br>" + "<br>")
+// }
+
+
+
+// question 4//
+
+
+
+// for ( var i= 1; i <=8; i++){
+//     document.write("ye aisy hi sof hoga " + "<br>" + "<br>")
+// }
+
+
+
+// question 5//
+
+
+
+
+// var  input = prompt("enter uour number");
+// var userInput = prompt("enter you last numbr");
+// for(var i = 0; i<= userInput; i++){
+// document.write(input + i + input +"<br>")
+// }
+
+// question 6//
+
+// // var userInput =prompt("What This")
+// var arr =[" "]
+// var userInput = prompt("enter your name")
+// if(userInput === userInput){
+//     alert ("welcome sir ")
+// }else alert("sorry sir ")
+
+
+
+// question 7
+
+// var arr =[ "hamza", "sami", "muddsir",]
+// for(var k =1; k<=10; k++){
+//     document.write("hello sir"+ k+"<br>")
+// }
+
+
+
+// // question 8//
+// var usercity = prompt("enter your city")
+// var userlenth = +prompt("enter you lenth")
+// for(var m =1; m <=userlenth; m++){
+//     document.write( usercity + m + "<br>")
+// } 
+
+
+
+
+// // question 9//
+// var userInput = prompt("Select you pets")
+// var userlenth = +prompt("Enter you types")
+// var arry = ["dog","elephent","tiger","hours","cat"]
+// if( arry=== arry)
+// for(var i=1; i<=userlenth; i++ )
+// {
+//     document.write(userInput + i + "<br>")
+// }else alert("plzz try again")
+
+
+
+
+
+
+// question 10//
+
+// var userInput = prompt("Chose this ")
+// var userlenth =+prompt("Enter your lenth")
+// for ( var i= 1; i <=10; i++){
+//     console.log(userInput + i + "<br>")
+
+// }
+
+
+
+
+
+// // question 11//
+
+// var usertable = +prompt("enter your number")
+// var multiplevalve = +prompt("emter you table number")
+// for(var a =1; a<=multiplevalve; a++){
+//     document.write(usertable + "x" + a + "=" + usertable* +  a + "<br>")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
